9aed54b3ddde37868c3dbf39130639c41f70ee97
Igor <igorediti@gmail.com>
2025-12-11 00:10:03 -0300

    corrigindo o Allow host

--- main/settings.py
+++ main/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nerumengenharia.onrender.com', 'localhost', '127.0.0.1']
-
-
+ALLOWED_HOSTS = ['nerum-engenharia.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
